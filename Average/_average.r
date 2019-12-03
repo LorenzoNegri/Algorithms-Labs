494b93806994311eb5420a8602de333e79a6e97b
Lorenzo Negri <lorenz.negri@gmail.com>
2019-12-03 19:45:54 +0100

    Update _average.r

--- Average/_average.r
+++ Average/_average.r
@@ -1,6 +1,6 @@
 # Basic Average function
 Average <- function(S, n) {
-  # function to print x raised to the power y
+  # function to compute average from an array S and n values
   x = 0
   for (i in seq(n)){
     x = x + S[i]
@@ -11,7 +11,7 @@ Average <- function(S, n) {
 
 # Intelligent Average function that takes only the array of values
 avg <- function(S) {
-  # function to print x raised to the power y
+  # function to compute average only from an array S of values
   x = 0
   for (i in 1:length(S)){
     x = x + S[i]
